3aff90f9283cce44d16c579c03da5d3cc92875b6
mohamedxyosry-netizen <mohamedxyosry@gmail.com>
2026-09-01 17:23:22 +0300

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://gzyybadyaunizdlvmcle.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_zg4OnUfu83dzvD_GOIgarA__gbaXCEO";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
